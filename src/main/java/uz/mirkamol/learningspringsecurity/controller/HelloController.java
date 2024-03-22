330879e13b13d8fbdb62354ea52c08aa895dbd6c
mirkamol <mirkamolasrorov5@gmail.com>
2024-03-23 02:32:32 +0500

    Set Up Spring Security Configuration

---
+++ src/main/java/uz/mirkamol/learningspringsecurity/controller/HelloController.java
@@ -0,0 +1,16 @@
+package uz.mirkamol.learningspringsecurity.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HelloController {
+
+    @GetMapping("/")
+    public String greeting(){
+        return "Hello, World";
+    }
+}
